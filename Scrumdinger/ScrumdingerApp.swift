516a3710fe53a8b43530afbfde1c2b74ee4e358a
Abdalrahim Al Ayubi <abdalrahim.abdullah1@gmail.com>
2020-12-15 20:43:55 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Esar Tech  on 15/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
