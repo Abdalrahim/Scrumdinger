7e2dde96e4793ef4ef4ff408542be9b5c8370177
Abdalrahim Al Ayubi <abdalrahim.abdullah1@gmail.com>
2020-12-26 22:29:57 +0300

    Managing State and Life Cycle

---
+++ Scrumdinger/Models/Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Extension.swift
+//  Scrumdinger
+//
+//  Created by Esar Tech  on 26/12/2020.
+//
+
+import Foundation
+import AVFoundation
+
+extension AVPlayer {
+    static let sharedDingPlayer: AVPlayer = {
+        guard let url = Bundle.main.url(forResource: "ding", withExtension: "wav") else { fatalError("Failed to find sound file.") }
+        return AVPlayer(url: url)
+    }()
+}
---
+++ Scrumdinger/Views/MeetingView/MeetingView.swift
@@ -0,0 +1,53 @@
+//
+//  MeetingView.swift
+//  Scrumdinger
+//
+//  Created by Esar Tech  on 15/12/2020.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    var player: AVPlayer { AVPlayer.sharedDingPlayer }
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.color)
+            VStack {
+                MeetingHeaderView
+                (
+                    secondsElapsed: $scrumTimer.secondsElapsed,
+                    secondsRemaining: $scrumTimer.secondsRemaining,
+                    scrumColor: scrum.color
+                )
+                
+                Circle()
+                    .strokeBorder(lineWidth: 24, antialiased: true)
+                MeetingFooterView(speakers: $scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
+            }
+            .padding()
+            //.foregroundColor(scrum.color)
+        }
+        .padding()
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.speakerChangedAction = {
+                player.seek(to: .zero)
+                player.play()
+            }
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView(scrum: .constant(DailyScrum.data[1]))
+    }
+}
