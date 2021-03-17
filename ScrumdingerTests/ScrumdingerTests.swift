a87d3c704243d3d85a72059ff5ad6c3b81b151d2
Abdalrahim Al Ayubi <abdalrahim.abdullah1@gmail.com>
2021-03-17 16:21:09 +0300

    Added tests Add Mac Support

--- ScrumdingerTests/ScrumdingerTests.swift
+++ ScrumdingerTests/ScrumdingerTests.swift
@@ -6,21 +6,46 @@
 //
 
 import XCTest
+import SwiftUI
 @testable import Scrumdinger
 
 class ScrumdingerTests: XCTestCase {
-
+    
+    var sut: ScrumdingerApp!
+    @ObservedObject private var data = ScrumData()
+    
+    let scrum = DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, color: Color(#colorLiteral(red: 0.1607843137, green: 0.6117647059, blue: 0.6588235294, alpha: 1)))
+    
+    var daily = DailyScrum.Data()
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        sut = ScrumdingerApp()
+        self.data.scrums.append(scrum)
+        self.data.save()
+        self.performIfLoggedIn {
+            print("closed")
+        }
     }
+    
+    var isLoggedIn = true
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        super.tearDown()
     }
 
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    private func performIfLoggedIn(closure: () -> Void) {
+        print("isLoggedIn ", isLoggedIn)
+        if isLoggedIn {
+            closure()
+        }
     }
 
     func testPerformanceExample() throws {
